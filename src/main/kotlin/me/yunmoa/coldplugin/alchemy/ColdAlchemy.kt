05f31e996ffe08e042e6bb0dd96b396e6e577dfe
Yunmoa <Yunmoa@outlook.com>
2023-08-26 23:39:16 +0800

    [1.0.0] commit

--- src/main/kotlin/me/yunmoa/coldplugin/alchemy/ColdAlchemy.kt
+++ src/main/kotlin/me/yunmoa/coldplugin/alchemy/ColdAlchemy.kt
@@ -3,7 +3,7 @@ package io.github.username.project
 import taboolib.common.platform.Plugin
 import taboolib.common.platform.function.info
 
-object ExamplePlugin : Plugin() {
+object ColdAlchemy : Plugin() {
 
     override fun onEnable() {
         info("Successfully running ExamplePlugin!")
